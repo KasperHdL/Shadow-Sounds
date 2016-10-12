ecdcde8c9dd1ff766aafa5dd33b0925f0372000d
¨KasperHdL¨ <kasper@honnens.dk>
2016-10-12 11:04:06 +0200

    Simple movement and audio system

---
+++ Assets/AudioHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AudioHandler : MonoBehaviour {
+    
+    public Transform player;
+    public List<AudioSource> sources;
+
+	void Update () {
+        for(int i = 0;i < sources.Count; i++){
+            if(sources[i].transform.position.x < player.position.x -.5f){
+                sources[i].panStereo = -1f;
+                sources[i].volume = 1f;
+            }else if(sources[i].transform.position.x > player.position.x + .5f){
+                sources[i].panStereo = 1f;
+                sources[i].volume = 1f;
+            }else{
+                sources[i].volume = 0f;
+            }
+        }
+	
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float forwardSpeed = 1;
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+
+        Vector3 d = Vector3.up * forwardSpeed * Time.deltaTime;
+        if(Input.GetKeyDown(leftKey))
+            d.x = -1f;
+        else if(Input.GetKeyDown(rightKey))
+            d.x = 1f;
+	
+        transform.position += d;
+	}
+
+    void OnCollisionEnter2D(Collision2D coll){
+        Debug.Log("test");
+
+    }
+}
