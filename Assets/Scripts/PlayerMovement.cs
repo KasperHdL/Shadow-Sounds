630e7be36566630e388d935e71e80a1016340573
¨KasperHdL¨ <kasper@honnens.dk>
2016-10-25 20:27:10 +0200

    Implemented Simple Movement and Camera System

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveForce = 500f;
+    private Rigidbody2D body;
+
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+	
+	}
+	
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+
+        body.AddForce(new Vector3(h,v,0) * moveForce * Time.deltaTime);
+
+	
+	}
+}
---
+++ Assets/Scripts/TrackingCamera.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackingCamera : MonoBehaviour {
+
+	public Rigidbody2D target;
+
+	public float smoothFactor = 0.8f;
+
+	public float offsetZ  = -10;
+	public float viewOffsetMultiplier = 2f;
+
+    void Start(){
+        if(target == null){
+            Debug.LogWarning("Camera has no target, gonna try to find an object tagged 'Player'");
+            target = GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>();
+            if(target == null)
+                Debug.LogError("No object tagged 'Player'");
+        }
+
+    }
+	
+	void FixedUpdate () {
+        
+        Vector3 delta = target.transform.position - transform.position;
+        delta.z = 0;
+        Vector3 desiredPosition = (Vector2)transform.position + (Vector2)delta.normalized * delta.sqrMagnitude + target.velocity * viewOffsetMultiplier;
+
+        desiredPosition.z = offsetZ;
+
+        delta = desiredPosition - transform.position;
+        transform.position += (Vector3)delta * smoothFactor * Time.deltaTime;
+
+
+	}
+}
