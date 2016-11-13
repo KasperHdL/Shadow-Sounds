8f8762c2b34cacc207fc216e1e1d28fbcd6425bb
Petros Ioannidis <petros.ioannidis91@gmail.com>
2016-11-13 20:31:03 +0100

    new hall added

--- Assets/Scripts/OpenCages.cs
+++ Assets/Scripts/OpenCages.cs
@@ -11,6 +11,7 @@ public class OpenCages : MonoBehaviour {
             foreach ( GameObject elem in cages){
                 Destroy(elem);
             }
+            Destroy(this);
         }
     }
 }
