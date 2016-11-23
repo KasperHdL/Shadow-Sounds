849da33f0b27860c25540e12ac0f738073e8394b
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2016-11-23 15:14:12 +0100

    New Button and Door

---
+++ Assets/Scripts/Environment/Button.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+    public enum ButtonMode{
+        ToggleDoor,
+        OpenDoor,
+        CloseDoor
+    };
+
+    public Door door;
+    public ButtonMode buttonMode;
+
+    void FixedUpdate(){
+        //State Inormation
+
+    }
+
+    void OnTriggerEnter2D(Collider2D coll){
+        if(coll.gameObject.tag == "Player"){
+            switch(buttonMode){
+                case ButtonMode.ToggleDoor:
+                    door.Toggle();
+                    break;
+                case ButtonMode.OpenDoor:
+                    door.Open();
+                    break;
+                case ButtonMode.CloseDoor:
+                    door.Close();
+                    break;
+            }
+        }
+    }
+}
