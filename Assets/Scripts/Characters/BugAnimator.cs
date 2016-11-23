a84297a7089c1db8a0db271b59257c449ceaac3d
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2016-11-23 16:26:19 +0100

    Fixed and Tweaked Legs

--- Assets/Scripts/Characters/BugAnimator.cs
+++ Assets/Scripts/Characters/BugAnimator.cs
@@ -26,6 +26,7 @@ public class BugAnimator : MonoBehaviour {
     private float[] rightLegOffsets;
 
     public float velocityFactor = 1;
+    public float legOffsetAngle = 0;
 
 
     public void Start()
@@ -37,19 +38,23 @@ public class BugAnimator : MonoBehaviour {
         rightJawOffset = Random.value * RandomJawOffset;
         leftLegOffsets = LeftLegs.Select(_ => Random.value * RandomLegOffset).ToArray();
         rightLegOffsets = RightLegs.Select(_ => Random.value * RandomLegOffset).ToArray();
+
     }
 
     public void Update() {
         LeftJaw.localEulerAngles = new Vector3(0, 0, AnimationJawFactor * JawCurve.Evaluate(Time.time * AnimationJawSpeed + leftJawOffset));
         RightJaw.localEulerAngles = new Vector3(0, 0, -AnimationJawFactor * JawCurve.Evaluate(Time.time * AnimationJawSpeed + rightJawOffset));
 
+
+
+
         float speedMultiplier = (enemy.attacking ? 2 : (rigidbody.velocity.magnitude == 0 ? 0 : 1)) * velocityFactor;
 
         float t = Time.time * AnimationLegSpeed * speedMultiplier;
 
         for (int i = 0; i < LeftLegs.Length; i++)
-            LeftLegs[i].localEulerAngles = new Vector3(0, 0, AnimationLegFactor*LegCurve.Evaluate(t + leftLegOffsets[i]));
+            LeftLegs[i].localEulerAngles = new Vector3(0, 0, AnimationLegFactor * LegCurve.Evaluate(t + leftLegOffsets[i]) + legOffsetAngle);
         for (int i = 0; i < RightLegs.Length; i++)
-            RightLegs[i].localEulerAngles = new Vector3(0, 0, -AnimationLegFactor*LegCurve.Evaluate(t) + rightLegOffsets[i]);
+            RightLegs[i].localEulerAngles = new Vector3(0, 0, AnimationLegFactor * -LegCurve.Evaluate(t + rightLegOffsets[i]) - legOffsetAngle);
     }
 }
