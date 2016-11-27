98bc5edf3e42940c0b0e0736434eb0452b16a74a
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2016-11-27 14:17:21 +0100

    Removed Debug Message

--- Assets/Scripts/Environment/ExitScene.cs
+++ Assets/Scripts/Environment/ExitScene.cs
@@ -6,7 +6,6 @@ public class ExitScene : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D enter){
         if(enter.gameObject.tag == "Player"){
-            Debug.LogWarning("Player collided");
             Scene scene = SceneManager.GetActiveScene(); 
             SceneManager.LoadScene(scene.name);
         }
