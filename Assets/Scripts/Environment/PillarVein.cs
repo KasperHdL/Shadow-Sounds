2a3bc6a7013c2027de0ab0963c5db07fce453cf4
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2016-11-30 14:17:38 +0100

    Fixed a bug

--- Assets/Scripts/Environment/PillarVein.cs
+++ Assets/Scripts/Environment/PillarVein.cs
@@ -43,6 +43,7 @@ public class PillarVein : MonoBehaviour {
         line = gameObject.AddComponent<LineRenderer>();
         if(root == null)
             root = GetComponent<Pillar>();
+        if (End == transform) End = null;
         if(End == null) {
             Destroy(this);
             return;
@@ -177,10 +178,10 @@ public class PillarVein : MonoBehaviour {
                 var o = (t % SectionLength) / SectionLength;
 
                 var pos = positions;
-                if(v >= positions.Length - 1) {
+                if(v >= positions.Length - 2) {
                     var end = End.GetComponent<PillarVein>();
                     if(end == null) {
-                        v = positions.Length-2;
+                        v = positions.Length - 2;
                         o = 1;
                     } else {
                         pos = end.positions;
@@ -189,6 +190,7 @@ public class PillarVein : MonoBehaviour {
                 }
                 if (v < 0) v = 0;
                 
+                //Debug.Log(v + " " + pos.Length);
                 aline.SetPosition(k < 0 ? k + 2 : (k >= psecs ? k - psecs : k), Vector3.LerpUnclamped(pos[v], pos[v + 1], o) + Vector3.back);
                 latest = Vector3.LerpUnclamped(pos[v], pos[v + 1], o);
             }
